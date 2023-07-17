2c2f0ab475563eec14648c4f2f3a4517e85e3418
ShyguyMatt <mjherford@gmail.com>
2023-07-17 19:35:07 -0400

    added sessions to server.js

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'verysecretstring',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
