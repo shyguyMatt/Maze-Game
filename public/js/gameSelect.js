7f76c9e6609ddcee90435f5f7e0d66370d5cab9a
ShyguyMatt <mjherford@gmail.com>
2023-07-22 18:11:29 -0400

    added some functionality to pages

---
+++ public/js/gameSelect.js
@@ -0,0 +1,11 @@
+const highscoreBtnHandler = async (event) => {
+    event.preventDefault();
+
+    const mapId = event.srcElement.attributes.maze_id.nodeValue
+    console.log(mapId)
+
+    document.location.replace(`/highscores/${mapId}`)
+}
+
+const highscoreBtns = document.querySelectorAll(".highscoreBtn").forEach(btn => 
+    btn.addEventListener('click', highscoreBtnHandler))
