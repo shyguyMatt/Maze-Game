a7a4f7989bcd2b6cc6b5c9480308b1d4acacabf1
ShyguyMatt <mjherford@gmail.com>
2023-07-23 23:01:14 -0400

    connected login routes to new login modal

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,5 @@
-const loginBtnHandler = async (event) => {
+const loginHandler = async (event) => {
     event.preventDefault();
-    console.log('button pressed')
 
     const email = document.querySelector('#loginEmail').value.trim();
     const password = document.querySelector('#loginPassword').value.trim();
@@ -14,13 +13,67 @@ const loginBtnHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.href =  '/';
-        alert('successfully logged in')
+        document.querySelector('#loginModal').classList.remove('is-active')
+        document.querySelector('#loginEmail').value = '';
+        document.querySelector('#loginPassword').value = '';
       } else {
         alert('Failed to log in');
       }
     }
   };
 
+const signupHandler = async (event) => {
+  event.preventDefault();
+
+  const newPlayerData = {
+    user_name: document.querySelector('#signupName').value.trim(),
+    email: document.querySelector('#signupEmail').value.trim(),
+    password: document.querySelector('#signupPassword').value.trim(),
+  }
+
+  response = await fetch('/api/user/newUser', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newPlayerData)
+  })
+
+  if (response.ok) {
+    document.querySelector('#signupModal').classList.remove('is-active')
+  }
+}
+
+const loginCancelBtns = document.querySelectorAll('.loginCancel').forEach(btn =>
+  btn.addEventListener('click', function() {
+  document.querySelector('#loginModal').classList.remove('is-active');
+  document.querySelector('#loginEmail').value = '';
+  document.querySelector('#loginPassword').value = '';
+}));
+
+const signupCancelBtns = document.querySelectorAll('.signupCancel').forEach(btn =>
+  btn.addEventListener('click', function() {
+  document.querySelector('#signupModal').classList.remove('is-active');
+  document.querySelector('#signupName').value = '';
+  document.querySelector('#signupEmail').value = '';
+  document.querySelector('#signupPassword').value = '';
+}));
+
+document.querySelector('#loginLink').addEventListener('click', function() {
+  document.querySelector('#signupModal').classList.remove('is-active');
+  document.querySelector('#signupName').value = '';
+  document.querySelector('#signupEmail').value = '';
+  document.querySelector('#signupPassword').value = '';
+  document.querySelector('#loginModal').classList.add('is-active');
+});
+
+document.querySelector('#signupLink').addEventListener('click', function() {
+  document.querySelector('#loginModal').classList.remove('is-active');
+  document.querySelector('#loginEmail').value = '';
+  document.querySelector('#loginPassword').value = '';
+  document.querySelector('#signupModal').classList.add('is-active');
+});
+
 document.querySelector('#loginBtn')
-        .addEventListener('click', loginBtnHandler)
+        .addEventListener('click', loginHandler)
+
+document.querySelector('#signupBtn')
+        .addEventListener('click', signupHandler)
